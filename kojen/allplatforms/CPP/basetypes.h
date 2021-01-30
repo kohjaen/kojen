e0e9c44239ba0fbd55e73fb416bab1445752584c
Eugene <koh.jaen@yahoo.de>
2021-01-30 16:24:22 +0100

    Update minunit for GCC.

--- kojen/allplatforms/CPP/basetypes.h
+++ kojen/allplatforms/CPP/basetypes.h
@@ -48,7 +48,7 @@
 // No GCC '#warning' in Visual Studio
 #define STRINGIZE_HELPER(x) #x
 #define STRINGIZE(x) STRINGIZE_HELPER(x)
-//#define WARNING(desc) message(__FILE__ "(" STRINGIZE(__LINE__) ") : Warning: " #desc)
+#define WARNING(desc) message(__FILE__ "(" STRINGIZE(__LINE__) ") : Warning: " #desc)
 //// usage:
 //#pragma WARNING(FIXME: Code removed because...)
 #elif defined(__GNUC__)
