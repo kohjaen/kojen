f61ca05591cde9bc3896a4b702770145375ad797
Eugene G <koh.jaen@yahoo.de>
2025-03-12 17:19:20 +0100

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.MD", "r") as fh:
 
 setuptools.setup(
     name="kojen",
-    version="1.3.42",
+    version="1.4.43",
     author="kohjaen",
     author_email="koh.jaen@yahoo.de",
     description="Code generation tools.",
